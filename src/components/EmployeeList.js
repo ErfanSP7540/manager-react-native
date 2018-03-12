bff6b1c250a23f5ecadbd34dcdce8da33bade3c3
erfanSP <erfansp@erfanSPs-Mac.local>
2018-03-12 20:47:35 +0330

    initial prop for Scene component

---
+++ src/components/EmployeeList.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Text , View } from 'react-native'
+
+
+const EmployeeList =()=>{
+    return (
+        <View>
+            <Text>Employeee List</Text>
+            <Text>Employeee List</Text>
+            <Text>Employeee List</Text>
+            <Text>Employeee List</Text>
+            <Text>Employeee List</Text>
+            <Text>Employeee List</Text>
+            <Text>Employeee List</Text>
+        </View>
+    )
+}
+
+export default EmployeeList 
--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 import React from 'react'
+
 import LoginForm from './components/LoginForm'
+import EmployeeList from './components/EmployeeList'
 
 import {Router,Scene,Stack} from 'react-native-router-flux'
 
@@ -7,9 +9,10 @@ import {Router,Scene,Stack} from 'react-native-router-flux'
 const RouterComponent=()=>{
     
     return(
-        <Router>
+        <Router >
             <Stack key="root">
-                <Scene key="login" component={LoginForm} title="Please Login"/>
+                <Scene key="login" component={LoginForm} title="Please Login" />
+                <Scene key="EmployeeList" component={EmployeeList} title="Employees" initial />
             </Stack>
         </Router>
     )
